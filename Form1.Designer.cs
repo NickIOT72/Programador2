72b8311fe2d9bf6fca4d9c11f5b4d0e5e3c74259
NickIOT72 <59583805+NickIOT72@users.noreply.github.com>
2020-02-25 09:17:46 -0400

    First commit
    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Diseño_Programador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PanelBarra = new System.Windows.Forms.Panel();
+            this.PanelMin = new System.Windows.Forms.Panel();
+            this.PanelMax = new System.Windows.Forms.Panel();
+            this.PanelSalir = new System.Windows.Forms.Panel();
+            this.PanelDetectar = new System.Windows.Forms.Panel();
+            this.Lbl = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SelPuerto = new System.Windows.Forms.Label();
+            this.RegresarDet = new System.Windows.Forms.Panel();
+            this.RegPantalla2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // PanelBarra
+            // 
+            this.PanelBarra.Location = new System.Drawing.Point(12, 12);
+            this.PanelBarra.Name = "PanelBarra";
+            this.PanelBarra.Size = new System.Drawing.Size(48, 23);
+            this.PanelBarra.TabIndex = 0;
+            this.PanelBarra.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.PanelBarra.MouseHover += new System.EventHandler(this.PanelBarra_MouseHover);
+            // 
+            // PanelMin
+            // 
+            this.PanelMin.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.PanelMin.Location = new System.Drawing.Point(88, 12);
+            this.PanelMin.Name = "PanelMin";
+            this.PanelMin.Size = new System.Drawing.Size(39, 25);
+            this.PanelMin.TabIndex = 1;
+            this.PanelMin.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PanelMin_MouseClick);
+            this.PanelMin.MouseLeave += new System.EventHandler(this.PanelMin_MouseLeave_1);
+            this.PanelMin.MouseHover += new System.EventHandler(this.PanelMin_MouseHover_1);
+            // 
+            // PanelMax
+            // 
+            this.PanelMax.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.PanelMax.Location = new System.Drawing.Point(148, 12);
+            this.PanelMax.Name = "PanelMax";
+            this.PanelMax.Size = new System.Drawing.Size(51, 26);
+            this.PanelMax.TabIndex = 2;
+            this.PanelMax.MouseLeave += new System.EventHandler(this.PanelMax_MouseLeave_1);
+            this.PanelMax.MouseHover += new System.EventHandler(this.PanelMax_MouseHover_1);
+            // 
+            // PanelSalir
+            // 
+            this.PanelSalir.BackColor = System.Drawing.SystemColors.Desktop;
+            this.PanelSalir.Location = new System.Drawing.Point(223, 12);
+            this.PanelSalir.Name = "PanelSalir";
+            this.PanelSalir.Size = new System.Drawing.Size(57, 29);
+            this.PanelSalir.TabIndex = 3;
+            this.PanelSalir.MouseLeave += new System.EventHandler(this.PanelSalir_MouseLeave_1);
+            this.PanelSalir.MouseHover += new System.EventHandler(this.PanelSalir_MouseHover_1);
+            // 
+            // PanelDetectar
+            // 
+            this.PanelDetectar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.PanelDetectar.Location = new System.Drawing.Point(299, 12);
+            this.PanelDetectar.Name = "PanelDetectar";
+            this.PanelDetectar.Size = new System.Drawing.Size(59, 29);
+            this.PanelDetectar.TabIndex = 4;
+            this.PanelDetectar.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelDetectar_Paint);
+            this.PanelDetectar.MouseClick += new System.Windows.Forms.MouseEventHandler(this.PanelDetectar_MouseClick);
+            this.PanelDetectar.MouseLeave += new System.EventHandler(this.PanelDetectar_MouseLeave);
+            this.PanelDetectar.MouseHover += new System.EventHandler(this.PanelDetectar_MouseHover);
+            // 
+            // Lbl
+            // 
+            this.Lbl.AutoSize = true;
+            this.Lbl.Location = new System.Drawing.Point(311, 44);
+            this.Lbl.Name = "Lbl";
+            this.Lbl.Size = new System.Drawing.Size(35, 13);
+            this.Lbl.TabIndex = 5;
+            this.Lbl.Text = "label1";
+            this.Lbl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Lbl_MouseClick);
+            this.Lbl.MouseLeave += new System.EventHandler(this.Lbl_MouseLeave);
+            this.Lbl.MouseHover += new System.EventHandler(this.Lbl_MouseHover);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // SelPuerto
+            // 
+            this.SelPuerto.AutoSize = true;
+            this.SelPuerto.Location = new System.Drawing.Point(383, 12);
+            this.SelPuerto.Name = "SelPuerto";
+            this.SelPuerto.Size = new System.Drawing.Size(35, 13);
+            this.SelPuerto.TabIndex = 6;
+            this.SelPuerto.Text = "label1";
+            this.SelPuerto.Click += new System.EventHandler(this.SelPuerto_Click);
+            // 
+            // RegresarDet
+            // 
+            this.RegresarDet.BackColor = System.Drawing.SystemColors.GrayText;
+            this.RegresarDet.Location = new System.Drawing.Point(18, 61);
+            this.RegresarDet.Name = "RegresarDet";
+            this.RegresarDet.Size = new System.Drawing.Size(42, 17);
+            this.RegresarDet.TabIndex = 7;
+            this.RegresarDet.MouseClick += new System.Windows.Forms.MouseEventHandler(this.RegresarDet_MouseClick);
+            // 
+            // RegPantalla2
+            // 
+            this.RegPantalla2.AutoSize = true;
+            this.RegPantalla2.Location = new System.Drawing.Point(15, 81);
+            this.RegPantalla2.Name = "RegPantalla2";
+            this.RegPantalla2.Size = new System.Drawing.Size(35, 13);
+            this.RegPantalla2.TabIndex = 8;
+            this.RegPantalla2.Text = "label1";
+            this.RegPantalla2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.RegPantalla2_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 261);
+            this.Controls.Add(this.RegPantalla2);
+            this.Controls.Add(this.RegresarDet);
+            this.Controls.Add(this.SelPuerto);
+            this.Controls.Add(this.Lbl);
+            this.Controls.Add(this.PanelDetectar);
+            this.Controls.Add(this.PanelSalir);
+            this.Controls.Add(this.PanelMax);
+            this.Controls.Add(this.PanelMin);
+            this.Controls.Add(this.PanelBarra);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelBarra;
+        private System.Windows.Forms.Panel PanelMin;
+        private System.Windows.Forms.Panel PanelMax;
+        private System.Windows.Forms.Panel PanelSalir;
+        private System.Windows.Forms.Panel PanelDetectar;
+        private System.Windows.Forms.Label Lbl;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label SelPuerto;
+        private System.Windows.Forms.Panel RegresarDet;
+        private System.Windows.Forms.Label RegPantalla2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,455 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Diseño_Programador
+{
+    public partial class Form1 : Form
+    {
+        int MedidaOpcion = 0;
+        int OpcionImagenTamaño = 0;
+        int EspacioAdicional = 20;
+
+        string LocationOfIntroImage = "H";
+
+        //Vraibles para el timer
+        bool AutorizarAnimacionDetectar = false;
+        bool AutorizarAnimacionLbl = false;
+        bool AutorizarContadorCambioDetLbl = false;
+        int ContadorAnimacionDetectar = 0;
+        int ValorMinAnimacionDetectar = 0;
+        int ValorMaxAnimacionDetectar = 100;
+        int ContadorCambioPanelDetectarLbl = 0;
+        int ValorMxContadorCambio = 5;
+        int StepAnimacion = 20;
+        bool AutorizarTraslacionDetectar = false;
+        int ContadorTraslacionDetectar = 0;
+        double ExponenecialContador = 0;
+        bool AutorizarTraslacionInversaDetectar = false;
+        bool AutorizarRegresarPantalla1 = false;
+        bool AutorizarTranslacionInvertidaPantalla1 = false;
+        int LastValue = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Dimensiones de la Forma
+            this.Height = 600; //800 pixeles de longitug horizontal
+            this.Width = 800; //1000 pixeles de longitud veritical
+
+            ActualizarPantalla();// Se actualizan cada componente del sistema
+            
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        public void ActualizarPantalla()
+        {
+            this.BackColor = Color.FromArgb(255, 255, 255, 255);
+            //Diseño Barra de opciones superior
+            this.PanelBarra.Visible = true;
+            this.PanelBarra.BackColor = Color.FromArgb(255, 0, 32, 96);
+            this.PanelBarra.Height = 5 * this.Height / 100;
+            this.PanelBarra.Width = this.Width;
+            this.PanelBarra.Location = new Point(0, 0);
+            //this.PanelBarra.BringToFront();
+
+            MedidaOpcion = this.PanelBarra.Height;
+            OpcionImagenTamaño = 30 * this.PanelBarra.Height / 30;
+
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directory = System.IO.Path.GetDirectoryName(location);
+
+            //string LocationOfIntroImage = directory + PantallaSeleccionOpciones(OpcionSeleccionada);
+
+            //Diseño Opcion Minimizar barra
+            MostrarPanelMinimo(directory, 0);
+
+            //Diseño Opcion Maximizar barra
+            MostrarPanelMaximo(directory, 1);
+
+            //Diseño Opcion Salir barra
+            MostrarPanelSalir(directory, 2);
+
+            MostarPanelDetectarPlaca();
+
+            MostrarSeleccionOpciones();
+            this.SelPuerto.Visible = false;
+
+            MostrarPanelRegPantalla1();
+            this.RegresarDet.Visible = false;
+        }
+
+        public void MostrarPanelRegPantalla1()
+        {
+            this.RegresarDet.Visible = true;
+            this.RegresarDet.BackColor = Color.FromArgb(255, 120, 120, 120);
+            this.RegresarDet.Height = this.Height / 10;
+            this.RegresarDet.Width = 22 * this.Width / 100;
+            this.RegresarDet.Location = new Point(-this.RegresarDet.Width, this.Height - 5*EspacioAdicional);
+            this.RegPantalla2.Text = "Regresar\n";
+            this.RegPantalla2.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.RegPantalla2.ForeColor = Color.FromArgb(255, 255, 255, 255);
+            this.RegPantalla2.BackColor = Color.FromArgb(255, 120, 120, 120);
+            //this.Lbl.Width = 7 * this.PanelDetectar.Width / 10;
+            //this.Lbl.Height =  7 * this.PanelDetectar.Height / 10;
+            this.RegPantalla2.Location = new Point(5 * this.PanelDetectar.Width / 100, 5 * this.PanelDetectar.Height / 100);
+            this.RegresarDet.Controls.Add(RegPantalla2);
+        }
+
+        public void MostarPanelDetectarPlaca()
+        {
+            this.PanelDetectar.Visible = true;
+            this.PanelDetectar.BackColor = Color.FromArgb(255, 0, 96, 110);
+            this.PanelDetectar.Height = this.Width/6;
+            this.PanelDetectar.Width = this.Height/3;
+            this.PanelDetectar.Location = new Point(EspacioAdicional, this.PanelSalir.Height + EspacioAdicional);
+            //Label Lbl = new Label();
+            this.Lbl.Text = "Detectar\nPlaca";
+            this.Lbl.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.Lbl.ForeColor = Color.FromArgb(255, 255, 255, 255);
+            this.Lbl.BackColor = Color.FromArgb(255, 0, 96, 110 );
+            //this.Lbl.Width = 7 * this.PanelDetectar.Width / 10;
+            //this.Lbl.Height =  7 * this.PanelDetectar.Height / 10;
+            this.Lbl.Location = new Point(5 * this.PanelDetectar.Width / 100, 5 * this.PanelDetectar.Height / 100);
+
+            this.PanelDetectar.Controls.Add(Lbl);
+            //LocationOfIntroImage = direct + PantallaSeleccionOpciones(a);
+            //this.PanelMin.Controls.Clear();
+            //ShowImages(LocationOfIntroImage, a);
+        }
+
+        public void ShowImages(string Location , int a)
+        {
+            /*
+            PictureBox pb = new PictureBox();
+            pb.SizeMode = PictureBoxSizeMode.AutoSize;
+            pb.Image = Image.FromFile(Location);
+            if (a == 0 || a == 3)
+            {
+                pb.Image = new Bitmap(pb.Image, this.PanelMin.Width, pb.Image.Height);
+                this.PanelMin.Controls.Add(pb);
+            }
+            else if (a == 1 || a == 4)
+            {
+                pb.Image = new Bitmap(pb.Image, this.PanelMax.Width, pb.Image.Height);
+                this.PanelMax.Controls.Add(pb);
+            }
+            else if (a == 2 || a==5)
+            {
+                pb.Image = new Bitmap(pb.Image, this.PanelSalir.Width, pb.Image.Height);
+                this.PanelSalir.Controls.Add(pb);
+            }
+            */
+
+            Image NewImage = new Bitmap(Location);
+            if (a == 0 || a == 3)
+            {
+                this.PanelMin.BackgroundImage = NewImage;
+            }
+            else if (a == 1 || a == 4)
+            {
+                this.PanelMax.BackgroundImage = NewImage;
+            }
+            else if (a == 2 || a == 5)
+            {
+                this.PanelSalir.BackgroundImage = NewImage;
+            }
+
+            //ScroolText.AutoScroll = true;
+            //ScroolText.Controls.Add(pb);
+        }
+
+        public static string PantallaSeleccionOpciones(int a)
+        {
+            string[] TextoOpciones = new string[13];
+            TextoOpciones[0] = "\\Imagenes\\OpcionMin.png";
+            TextoOpciones[1] = "\\Imagenes\\OpcionMax.png";
+            TextoOpciones[2] = "\\Imagenes\\OpcionSalir.png";
+            TextoOpciones[3] = "\\Imagenes\\OpcionMinHover.png";
+            TextoOpciones[4] = "\\Imagenes\\OpcionMaxHover.png";
+            TextoOpciones[5] = "\\Imagenes\\OpcionSalirHover.png";
+
+            if (a == -1)
+            {
+                return "\\Imagenes\\Intro.png";
+            }
+            else
+            {
+                return TextoOpciones[a];
+            }
+        }
+
+        public void MostrarPanelMinimo(string direct , int a)
+        {
+            this.PanelMin.Visible = true;
+            //this.PanelMin.BackColor = Color.FromArgb(255, 255, 0, 0);
+            this.PanelMin.Height = MedidaOpcion;
+            this.PanelMin.Width = MedidaOpcion;
+            this.PanelMin.Location = new Point(this.Width - 3 * OpcionImagenTamaño - EspacioAdicional, 0);
+            LocationOfIntroImage = direct + PantallaSeleccionOpciones(a);
+            this.PanelMin.Controls.Clear();
+            ShowImages(LocationOfIntroImage, a);
+        }
+
+        public void MostrarPanelMaximo(string direct, int a)
+        {
+            this.PanelMax.Visible = true;
+            //this.PanelMin.BackColor = Color.FromArgb(255, 255, 0, 0);
+            this.PanelMax.Height = MedidaOpcion;
+            this.PanelMax.Width = MedidaOpcion;
+            this.PanelMax.Location = new Point(this.Width - 2 * OpcionImagenTamaño - EspacioAdicional, 0);
+            LocationOfIntroImage = direct + PantallaSeleccionOpciones(a);
+            this.PanelMax.Controls.Clear();
+            ShowImages(LocationOfIntroImage, a);
+        }
+
+        public void MostrarPanelSalir(string direct, int a)
+        {
+            this.PanelSalir.Visible = true;
+            //this.PanelMin.BackColor = Color.FromArgb(255, 255, 0, 0);
+            this.PanelSalir.Height = MedidaOpcion;
+            this.PanelSalir.Width = MedidaOpcion;
+            this.PanelSalir.Location = new Point(this.Width - 1 * OpcionImagenTamaño - EspacioAdicional, 0);
+            LocationOfIntroImage = direct + PantallaSeleccionOpciones(a);
+            this.PanelSalir.Controls.Clear();
+            ShowImages(LocationOfIntroImage, a);
+        }
+
+        // *****************************************************************
+        private void PanelMin_MouseHover_1(object sender, EventArgs e)
+        {
+            // Update the mouse event label to indicate the MouseHover event occurred.
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directory = System.IO.Path.GetDirectoryName(location);
+            MostrarPanelMinimo(directory, 3);
+            Console.WriteLine("Panel Minimo 1");
+        }
+
+        private void PanelMin_MouseLeave_1(object sender, EventArgs e)
+        {
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directory = System.IO.Path.GetDirectoryName(location);
+            // Update the mouse event label to indicate the MouseLeave event occurred.
+            MostrarPanelMinimo(directory, 0);
+            Console.WriteLine("Panel Minimo 0");
+        }
+
+        private void PanelMax_MouseHover_1(object sender, EventArgs e)
+        {
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directory = System.IO.Path.GetDirectoryName(location);
+            // Update the mouse event label to indicate the MouseHover event occurred.
+            MostrarPanelMinimo(directory, 4);
+        }
+
+        private void PanelMax_MouseLeave_1(object sender, EventArgs e)
+        {
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directory = System.IO.Path.GetDirectoryName(location);
+            // Update the mouse event label to indicate the MouseLeave event occurred.
+            MostrarPanelMinimo(directory, 1);
+        }
+
+        private void PanelSalir_MouseHover_1(object sender, EventArgs e)
+        {
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directory = System.IO.Path.GetDirectoryName(location);
+            // Update the mouse event label to indicate the MouseHover event occurred.
+            MostrarPanelMinimo(directory, 5);
+        }
+
+        private void PanelSalir_MouseLeave_1(object sender, EventArgs e)
+        {
+            var location = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string directory = System.IO.Path.GetDirectoryName(location);
+            // Update the mouse event label to indicate the MouseLeave event occurred.
+            MostrarPanelMinimo(directory, 2);
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void PanelMin_MouseClick(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Panel Minimo Pulsado");
+        }
+
+        private void PanelBarra_MouseHover(object sender, EventArgs e)
+        {
+            Console.WriteLine("Panel Barra 1");
+        }
+
+        private void PanelDetectar_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void PanelDetectar_MouseHover(object sender, EventArgs e)
+        {
+            AutorizarAnimacionDetectar = true;
+        }
+
+        public void AnimacionPanelDetectar()
+        {
+             //this.PanelDetectar.Width = this.PanelDetectar.Width + (a)*this.PanelDetectar.Width / 1000;
+             this.PanelDetectar.BackColor = Color.FromArgb(255, 0, 96, 110 + ContadorAnimacionDetectar);
+             this.Lbl.BackColor = this.PanelDetectar.BackColor;
+        }
+
+        private void PanelDetectar_MouseLeave(object sender, EventArgs e)
+        {
+            AutorizarAnimacionDetectar = false;          
+        }
+
+        private void Lbl_MouseHover(object sender, EventArgs e)
+        {
+            //AnimacionPanelDetectar();
+            AutorizarAnimacionDetectar = true;
+        }
+
+        private void Lbl_MouseLeave(object sender, EventArgs e)
+        {
+            AutorizarAnimacionDetectar = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool RecuadroDetectar = (Control.MousePosition.X >= (this.Left + 2 * this.PanelDetectar.Location.X) && Control.MousePosition.X <= (this.PanelDetectar.Width + this.Left + this.PanelDetectar.Location.X) && Control.MousePosition.Y >= (15 * this.PanelDetectar.Location.Y / 10 + this.Top) && Control.MousePosition.Y <= (8 * this.PanelDetectar.Location.Y / 5 + this.PanelDetectar.Height + this.Top));
+            if ( !AutorizarTraslacionInversaDetectar && !AutorizarTraslacionDetectar && (AutorizarAnimacionDetectar || RecuadroDetectar) )
+            {
+                if (ContadorAnimacionDetectar <= ValorMaxAnimacionDetectar)
+                {
+                    ContadorAnimacionDetectar += StepAnimacion;
+                    AnimacionPanelDetectar();
+                }
+            }
+            else if (!AutorizarTraslacionInversaDetectar && !AutorizarTraslacionDetectar && !AutorizarAnimacionDetectar && ContadorAnimacionDetectar > 0 && !RecuadroDetectar)
+            {
+                ContadorAnimacionDetectar -= 10;
+                AnimacionPanelDetectar();
+            }
+            else if (!AutorizarTraslacionInversaDetectar && AutorizarTraslacionDetectar)
+            {
+                TraslacionPanelDetectar();
+            }
+            if (AutorizarTraslacionInversaDetectar)
+            {
+                if (this.SelPuerto.Location.X < 8)
+                {
+                    ExponenecialContador++;
+                    int dat = this.SelPuerto.Location.X +((((int)(Math.Exp((ExponenecialContador / 8))))) );
+                    this.SelPuerto.Location = new Point(dat, this.SelPuerto.Location.Y);
+                    this.RegresarDet.Location = new Point(dat, this.Height - 8 * EspacioAdicional);
+                }
+                else if (this.SelPuerto.Location.X >= 8)
+                {
+                    ExponenecialContador = 0;
+                    AutorizarTraslacionInversaDetectar = false;
+ 
+                }
+            }
+            if (AutorizarRegresarPantalla1)
+            {
+                TraslacionRegresarPantalla1();
+            }
+
+        }
+
+        public void TraslacionRegresarPantalla1()
+        {
+            if (this.RegresarDet.Location.X > -this.RegresarDet.Width)
+            {
+                ExponenecialContador++;
+                int dat = this.PanelDetectar.Location.X - (int)(Math.Exp(ExponenecialContador / 8));
+                this.SelPuerto.Location = new Point(dat, this.SelPuerto.Location.Y);
+                this.RegresarDet.Location = new Point(dat, this.Height - 8 * EspacioAdicional);
+            }
+            else if (this.RegresarDet.Location.X <= -this.RegresarDet.Width)
+            {
+                this.PanelDetectar.Visible = false;
+                //LastValue = (int)(Math.Exp(ExponenecialContador / 8));
+                Console.WriteLine(LastValue.ToString());
+                ExponenecialContador = 0;
+                AutorizarRegresarPantalla1 = false;
+                this.SelPuerto.Visible = true;
+                this.RegresarDet.Visible = true;
+                //MostrarSeleccionOpciones();
+            }
+        }
+
+        public void TraslacionPanelDetectar()
+        {
+            if (this.PanelDetectar.Location.X > -this.PanelDetectar.Width)
+            {
+                ExponenecialContador++;
+                int dat = this.PanelDetectar.Location.X - (int)(Math.Exp(ExponenecialContador/8) );
+                this.PanelDetectar.Location = new Point(dat , this.PanelDetectar.Location.Y);
+            }
+            else if (this.PanelDetectar.Location.X <= -this.PanelDetectar.Width)
+            {
+                this.PanelDetectar.Visible = false;
+                LastValue = (int)(Math.Exp(ExponenecialContador / 8));
+                Console.WriteLine(LastValue.ToString());
+                ExponenecialContador = 0;
+                AutorizarTraslacionInversaDetectar = true;
+                this.SelPuerto.Visible = true;
+                this.RegresarDet.Visible = true;
+                //MostrarSeleccionOpciones();
+            }
+        }
+
+        public void MostrarSeleccionOpciones()
+        {
+            this.SelPuerto.Visible = true;
+            this.SelPuerto.Text = "Seleccione\nPuerto\nSerial";
+            this.SelPuerto.Font = new Font("Arial", 24, FontStyle.Bold);
+            this.SelPuerto.ForeColor = Color.FromArgb(255, 255, 255, 255);
+            this.SelPuerto.BackColor = Color.FromArgb(255, 0, 96, 110);
+            this.SelPuerto.Width = 7 * this.PanelDetectar.Width / 10;
+            this.SelPuerto.Height = 7 * this.PanelDetectar.Height / 10;
+            this.SelPuerto.Location = new Point(-this.SelPuerto.Width, this.PanelDetectar.Location.Y);
+        }
+
+        private void PanelDetectar_MouseClick(object sender, MouseEventArgs e)
+        {
+            AutorizarTraslacionDetectar = true;
+        }
+
+        private void Lbl_MouseClick(object sender, MouseEventArgs e)
+        {
+            AutorizarTraslacionDetectar = true;
+        }
+
+        private void SelPuerto_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegresarDet_MouseClick(object sender, MouseEventArgs e)
+        {
+            AutorizarRegresarPantalla1 = true;
+        }
+
+        private void RegPantalla2_MouseClick(object sender, MouseEventArgs e)
+        {
+            AutorizarRegresarPantalla1 = true;
+        }
+    }
+}
